86b1dc2c3a071ce5aaaec8723a8c047f6d418502
Daniel Wang <xwang@webmd.net>
2017-11-27 15:16:56 -0500

    xiaoshu7

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+class Header extends Component {
+
+
+    renderGoodWord(goodWord,badWord){
+        const isGoodWord = true;
+        return isGoodWord ? goodWord : badWord;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>React 小书
+                    {
+                        this.renderGoodWord(<strong> is good</strong>,<span> is bad</span>)
+                    }
+                </h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Header/>,
+    document.getElementById('root')
+)
