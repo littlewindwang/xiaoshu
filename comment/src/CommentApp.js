35b1bee03f142e97cbf087836effa9436207d6f2
Daniel Wang <xwang@webmd.net>
2017-12-14 16:28:59 -0500

    还没写完

--- comment/src/CommentApp.js
+++ comment/src/CommentApp.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react'
 
+import CommentInput from './CommentInput'
+
+class CommentApp extends CommentInput{
+    render(){
+        return(
+            <div>
+                <CommentInput/>
+            </div>
+        );
+    }
+}
